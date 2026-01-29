55891ef739876089cbec4978e6d4ff0055545dfe
ifs21011-itdel <archicosemb@gmail.com>
2026-01-29 21:38:58 +0700

    Update database.go

--- config/database.go
+++ config/database.go
@@ -12,23 +12,33 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
+	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found, using system environment variables")
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_NAME"),
+	// Ambil data dari env
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT") // Tambahan Port
+	dbName := os.Getenv("DB_NAME")
+
+	// Format DSN (Data Source Name) MySQL
+	// referensi: user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName,
 	)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect to database!")
+		// Panic jika database tidak konek, karena backend tidak bisa jalan tanpa DB
+		panic("Failed to connect to database: " + err.Error())
 	}
 
+	// Auto Migrate (Sama seperti sebelumnya)
+	log.Println("Database connected successfully, running migrations...")
 	err = db.AutoMigrate(
 		&entity.User{},
 		&entity.Course{},
